60a23f72abe85f0ace598f8d66ad26d6020eefe7
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2023-04-28 19:42:59 +0300

    Init prepare

---
+++ ITCareerProject.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITCareerProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=IT-Career-Exam;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
